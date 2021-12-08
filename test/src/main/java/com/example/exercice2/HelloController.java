883f400c48883a60fd583e92573bfd29bbf97b96
allantarcy <79646756+allantarcy@users.noreply.github.com>
2021-12-08 15:17:20 +0100

    updates

--- test/src/main/java/com/example/exercice2/HelloController.java
+++ test/src/main/java/com/example/exercice2/HelloController.java
@@ -1,12 +1,25 @@
 package com.example.exercice2;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.stage.FileChooser;
 
 public class HelloController {
 
     @FXML
-    FileChooser fc;
+    private FileChooser fc;
+    private ObservableList<String> liste = FXCollections.observableArrayList("item1","item2","item3","...");
+    @FXML
+    private ListView<String> items;
+
+    @Override
+    public void initialize(){
+        items.setItems(liste);
+        System.out.println("Done");
+    }
+
 
 }
