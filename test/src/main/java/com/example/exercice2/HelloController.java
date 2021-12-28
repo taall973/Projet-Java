43642945cec3a4bff82dc4dbecc7ca8cddaa89ce
allantarcy <79646756+allantarcy@users.noreply.github.com>
2021-12-28 17:46:09 +0100

    updates

--- test/src/main/java/com/example/exercice2/HelloController.java
+++ test/src/main/java/com/example/exercice2/HelloController.java
@@ -1,28 +1,70 @@
 package com.example.exercice2;
 
+import java.awt.Desktop;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Menu;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class HelloController {
 
+    private Desktop desktop = Desktop.getDesktop();
+
+    @FXML
+    BorderPane bp;
+    @FXML
+    Stage stage;
     @FXML
     private FileChooser fc;
-    private ObservableList<String> liste = FXCollections.observableArrayList("item1","item2","item3","...");
     @FXML
-    private ListView<String> items;
+    private Menu pickImage;
+    private ObservableList<String> liste = FXCollections.observableArrayList("item1", "item2", "item3", "...");
+    @FXML
+    private ListView<File> items;
     @FXML
     private TextField text;
 
     @FXML
-    protected void initialize(){
-        items.setItems(liste);
+    protected void initialize() {
+        //items.setItems(liste);
+        stage = (Stage) bp.getScene().getWindow();
         System.out.println("Done");
     }
 
+    @FXML
+    protected void chooseFile() {
+        pickImage.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                File file = fc.showOpenDialog(stage);
+                if (file != null) {
+                    openFile(file);
+                    List<File> files = Arrays.asList(file);
+                    ObservableList<File> listF = (ObservableList<File>) files;
+                    items.setItems(listF);
+                }
+            }
+        });
+    }
+
+    private void openFile(File file) {
+        try {
+            this.desktop.open(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
--- test/src/main/java/module-info.java
+++ test/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.exercice2 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.example.exercice2 to javafx.fxml;
