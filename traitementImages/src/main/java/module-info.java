3567e8849ecaeef1751d0e3f7e138f254ebb5f40
allantarcy <79646756+allantarcy@users.noreply.github.com>
2021-12-29 12:28:45 +0100

    update

--- traitementImages/src/main/java/module-info.java
+++ traitementImages/src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.example.traitementimages {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.example.traitementimages to javafx.fxml;
     exports com.example.traitementimages;
 }
