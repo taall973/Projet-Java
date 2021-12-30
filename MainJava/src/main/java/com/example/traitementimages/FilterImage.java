153fd6d9e8e64871cda1fd6e8cd4618cd8af75f8
Sefyupowa <88197900+Sefyupowa@users.noreply.github.com>
2021-12-30 15:27:30 +0100

    projetJava
    update

---
+++ MainJava/src/main/java/com/example/traitementimages/FilterImage.java
@@ -0,0 +1,224 @@
+package com.example.traitementimages;
+
+import javafx.scene.image.*;
+import javafx.scene.paint.Color;
+
+import java.io.File;
+
+public class FilterImage {
+    private Image image;
+    private int[] inputPixels, outputPixels;
+    private int height, width;
+    private WritableImage writableImage;
+    private PixelWriter pixelWriter;
+    private PixelReader pixelReader;
+    private int red , blue , green , opacity ;
+
+    public FilterImage(Image image) {
+        this.image = image;
+        width = (int) image.getWidth();
+        height = (int) image.getHeight();
+        inputPixels = new int[width * height * 4];
+        pixelReader = image.getPixelReader();
+        pixelReader.getPixels(0, 0, width, height, PixelFormat.getIntArgbInstance(), inputPixels, 0, width * 4);
+        writableImage = new WritableImage(pixelReader, width, height);
+    }
+
+    public void changeRGB ( int p) {
+        this.opacity = ((p >> 24) & 0xff);
+        this.red = ((p >> 16) & 0xff);
+        this.green = ((p >> 8) & 0xff);
+        this. blue = (p & 0xff);
+    }
+    /*public Image toBRG() {
+        PixelReader pixelReader = image.getPixelReader();
+        int height = (int) image.getHeight();
+        int width = (int) image.getWidth();
+        WritableImage writableImage = new WritableImage(pixelReader, width, height);
+        PixelWriter pixelWriter = writableImage.getPixelWriter();
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int pixel = pixelReader.getArgb(i, j);
+                int opacity = ((pixel >> 24) & 0xff);
+                int red = ((pixel >> 16) & 0xff);
+                int green = ((pixel >> 8) & 0xff);
+                int blue = ((pixel & 0xff));
+
+                int brg = ((opacity << 24) + (blue << 16) + (red << 8) + green);
+                pixelWriter.setArgb(i, j, brg);
+            }
+        }
+        return writableImage;
+    }*/
+
+    public Image toBRG() {
+        pixelWriter = writableImage.getPixelWriter();
+        outputPixels = new int[width * height * 4];
+        int i = 0;
+        int brg =0 ;
+        for (int pixel : inputPixels) {
+            changeRGB(pixel) ;
+            brg = ((this.opacity << 24) + (this.blue << 16) + (this.red << 8) + this.green);
+            outputPixels[i++] = brg;
+        }
+        pixelWriter.setPixels(0, 0, width, height, PixelFormat.getIntArgbInstance(), outputPixels, 0, width * 4);
+        return writableImage;
+    }
+
+    public Image toBlackAndWhite() {
+        PixelReader pixelReader = image.getPixelReader();
+        int height = (int) image.getHeight();
+        int width = (int) image.getWidth();
+        WritableImage writableImage = new WritableImage(pixelReader, width, height);
+        PixelWriter pixelWriter = writableImage.getPixelWriter();
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int pixel = pixelReader.getArgb(i, j);
+                int opacity = ((pixel >> 24) & 0xff);
+                int red = ((pixel >> 16) & 0xff);
+                int green = ((pixel >> 8) & 0xff);
+                int blue = ((pixel & 0xff));
+
+                int sum = ((opacity << 24) + (((red + green + blue) / 3) << 16) + (((red + green + blue) / 3) << 8) + ((red + green + blue) / 3));
+                pixelWriter.setArgb(i, j, sum);
+            }
+        }
+        return writableImage;
+    }
+
+    public Image toSepia() {
+        PixelReader pixelReader = image.getPixelReader();
+        int height = (int) image.getHeight();
+        int width = (int) image.getWidth();
+        WritableImage writableImage = new WritableImage(pixelReader, width, height);
+        PixelWriter pixelWriter = writableImage.getPixelWriter();
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int pixel = pixelReader.getArgb(i, j);
+                int opacity = ((pixel >> 24) & 0xff);
+                int red = ((pixel >> 16) & 0xff);
+                int green = ((pixel >> 8) & 0xff);
+                int blue = ((pixel & 0xff));
+
+                int redSepia = (int) ((red * .393) + (green * .769) + (blue * .189));
+                if (redSepia > 255) {
+                    redSepia = 255;
+                }
+                int greenSepia = (int) ((red * .349) + (green * .686) + (blue * .168));
+                if (greenSepia > 255) {
+                    greenSepia = 255;
+                }
+                int blueSepia = (int) ((red * .272) + (green * .534) + (blue * .131));
+                if (blueSepia > 255) {
+                    blueSepia = 255;
+                }
+                int sepia = (opacity << 24) + (redSepia << 16) + (greenSepia << 8) + blueSepia;
+                pixelWriter.setArgb(i, j, sepia);
+            }
+        }
+        return writableImage;
+    }
+
+    public Image toPrewitt() {
+        Image blackAndWhite = toBlackAndWhite();
+        PixelReader pixelReader = blackAndWhite.getPixelReader();
+        int height = (int) blackAndWhite.getHeight();
+        int width = (int) blackAndWhite.getWidth();
+        WritableImage writableImage = new WritableImage(pixelReader, width, height);
+        PixelWriter pixelWriter = writableImage.getPixelWriter();
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int intensity[][] = new int[3][3]; //ce tableau contient les valeurs des pixels de l'image nécessaires à la convolution
+                intensity[1][1] = pixelReader.getArgb(i, j) & 0xff;
+                if (i == 0) {
+                    intensity[0][0] = 0;
+                    intensity[0][1] = 0;
+                    intensity[0][2] = 0;
+                    intensity[2][1] = pixelReader.getArgb(i + 1, j) & 0xff;
+                    if (j == 0) {
+                        intensity[1][0] = 0;
+                        intensity[1][2] = pixelReader.getArgb(i, j + 1) & 0xff;
+                        intensity[2][0] = 0;
+                        intensity[2][2] = pixelReader.getArgb(i + 1, j + 1) & 0xff;
+                    } else if (j == height - 1) {
+                        intensity[1][0] = pixelReader.getArgb(i, j - 1) & 0xff;
+                        intensity[1][2] = 0;
+                        intensity[2][0] = pixelReader.getArgb(i + 1, j - 1) & 0xff;
+                        intensity[2][2] = 0;
+                    } else {
+                        intensity[1][0] = pixelReader.getArgb(i, j - 1) & 0xff;
+                        intensity[1][1] = pixelReader.getArgb(i, j) & 0xff;
+                        intensity[1][2] = pixelReader.getArgb(i, j + 1) & 0xff;
+                        intensity[2][0] = pixelReader.getArgb(i + 1, j - 1) & 0xff;
+                        intensity[2][2] = pixelReader.getArgb(i + 1, j + 1) & 0xff;
+                    }
+                } else if (i == width - 1) {
+                    intensity[2][0] = 0;
+                    intensity[2][1] = 0;
+                    intensity[2][2] = 0;
+                    intensity[0][1] = pixelReader.getArgb(i - 1, j) & 0xff;
+                    if (j == 0) {
+                        intensity[0][0] = 0;
+                        intensity[0][2] = pixelReader.getArgb(i - 1, j + 1) & 0xff;
+                        intensity[1][0] = 0;
+                        intensity[1][2] = pixelReader.getArgb(i, j + 1) & 0xff;
+                    } else if (j == height - 1) {
+                        intensity[0][0] = pixelReader.getArgb(i - 1, j - 1) & 0xff;
+                        intensity[0][2] = 0;
+                        intensity[1][0] = pixelReader.getArgb(i, j - 1) & 0xff;
+                        intensity[1][2] = 0;
+                    } else {
+                        intensity[0][0] = pixelReader.getArgb(i - 1, j - 1) & 0xff;
+                        intensity[0][2] = pixelReader.getArgb(i - 1, j + 1) & 0xff;
+                        intensity[1][0] = pixelReader.getArgb(i, j - 1) & 0xff;
+                        intensity[1][2] = pixelReader.getArgb(i, j + 1) & 0xff;
+                    }
+                } else {
+                    intensity[0][1] = pixelReader.getArgb(i - 1, j) & 0xff;
+                    intensity[2][1] = pixelReader.getArgb(i + 1, j) & 0xff;
+                    if (j == 0) {
+                        intensity[0][0] = 0;
+                        intensity[0][2] = pixelReader.getArgb(i - 1, j + 1) & 0xff;
+                        intensity[1][0] = 0;
+                        intensity[1][2] = pixelReader.getArgb(i, j + 1) & 0xff;
+                        intensity[2][0] = 0;
+                        intensity[2][2] = pixelReader.getArgb(i + 1, j + 1) & 0xff;
+                    } else if (j == height - 1) {
+                        intensity[0][0] = pixelReader.getArgb(i - 1, j - 1) & 0xff;
+                        intensity[0][2] = 0;
+                        intensity[1][0] = pixelReader.getArgb(i, j - 1) & 0xff;
+                        intensity[1][2] = 0;
+                        intensity[2][0] = pixelReader.getArgb(i + 1, j - 1) & 0xff;
+                        intensity[2][2] = 0;
+                    } else {
+                        intensity[0][0] = pixelReader.getArgb(i - 1, j - 1) & 0xff;
+                        intensity[0][2] = pixelReader.getArgb(i - 1, j + 1) & 0xff;
+                        intensity[1][0] = pixelReader.getArgb(i, j - 1) & 0xff;
+                        intensity[1][2] = pixelReader.getArgb(i, j + 1) & 0xff;
+                        intensity[2][0] = pixelReader.getArgb(i + 1, j - 1) & 0xff;
+                        intensity[2][2] = pixelReader.getArgb(i + 1, j + 1) & 0xff;
+                    }
+                }
+                int opacity = ((intensity[1][1] >> 24) & 0xff);
+
+                int gX = intensity[0][0] * 1 + intensity[0][1] * 0 + intensity[0][2] * -1 +
+                        intensity[1][0] * 1 + intensity[1][1] * 0 + intensity[1][2] * -1 +
+                        intensity[2][0] * 1 + intensity[2][1] * 0 + intensity[2][2] * -1;
+                int gY = intensity[0][0] * 1 + intensity[0][1] * 1 + intensity[0][2] * 1 +
+                        intensity[1][0] * 0 + intensity[1][1] * 0 + intensity[1][2] * 0 +
+                        intensity[2][0] * -1 + intensity[2][1] * -1 + intensity[2][2] * -1;
+                int g = (int) Math.sqrt(Math.pow(gX, 2) + Math.pow(gY, 2));
+                if (g > 255) {
+                    g = 255;
+                }
+                int prewitt = (255 << 24) + (g << 16) + (g << 8) + g;
+                pixelWriter.setArgb(i, j, prewitt);
+            }
+        }
+        return writableImage;
+    }
+}
---
+++ MainJava/src/main/java/com/example/traitementimages/HelloController.java
@@ -0,0 +1,133 @@
+package com.example.traitementimages;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.Arrays;
+import java.util.List;
+
+public class HelloController {
+
+    private FilterImage filterImage;
+    Stage stage;
+    @FXML
+    private FileChooser fc;
+    @FXML
+    private MenuItem pickImage;
+    private ObservableList<String> liste = FXCollections.observableArrayList();
+    @FXML
+    private ListView<String> items;
+    @FXML
+    private TextField text;
+    @FXML
+    private ImageView imageView, blueRedGreen, blackAndWhite, sepia, prewitt;
+
+    @FXML
+    protected void initialize() {
+        loadImages();
+        fc = new FileChooser();
+    }
+
+    @FXML
+    protected void chooseFile(ActionEvent actionEvent) throws IOException {
+        File file = fc.showOpenDialog(stage);
+        if (file != null) {
+            boolean unique = true;
+            for (String elmt : liste) {
+                if (elmt.equals(file.getName())) {
+                    Alert sameName = new Alert(Alert.AlertType.INFORMATION, "Un fichier enregistré à ce nom existe déjà !");
+                    sameName.setTitle("Doublon détecté");
+                    sameName.setHeaderText(null);
+                    sameName.show();
+                    unique = false;
+                }
+            }
+            if (unique) {
+                liste.add(file.getName());
+            }
+            items.setItems(liste);
+            Path source = Paths.get(file.getAbsolutePath());
+            Path dest = Paths.get("src/main/resources/images/" + file.getName());
+            Files.copy(source, dest, StandardCopyOption.REPLACE_EXISTING);
+            Image image = new Image(dest.toFile().toURI().toURL().toString());
+            filterImage = new FilterImage(image);
+            imageView.setImage(image);
+            blueRedGreen.setImage(filterImage.toBRG());
+            blackAndWhite.setImage(filterImage.toBlackAndWhite());
+            sepia.setImage(filterImage.toSepia());
+            prewitt.setImage(filterImage.toPrewitt());
+        }
+    }
+
+    @FXML
+    public void showImage() {
+        Image image = getCurrentImage();
+        filterImage = new FilterImage(image);
+        imageView.setImage(image);
+        blueRedGreen.setImage(filterImage.toBRG());
+        blackAndWhite.setImage(filterImage.toBlackAndWhite());
+        sepia.setImage(filterImage.toSepia());
+        prewitt.setImage(filterImage.toPrewitt());
+    }
+
+    @FXML
+    public void blueRedGreenFilter() {
+        Image currentImage = getCurrentImage();
+        filterImage = new FilterImage(currentImage);
+        imageView.setImage(filterImage.toBRG());
+    }
+
+    @FXML
+    public void blackAndWhiteFilter() {
+        Image currentImage = getCurrentImage();
+        filterImage = new FilterImage(currentImage);
+        imageView.setImage(filterImage.toBlackAndWhite());
+    }
+
+    @FXML
+    public void sepiaFilter() {
+        Image currentImage = getCurrentImage();
+        filterImage = new FilterImage(currentImage);
+        imageView.setImage(filterImage.toSepia());
+    }
+
+    @FXML
+    public void prewittFilter() {
+        Image currentImage = getCurrentImage();
+        filterImage = new FilterImage(currentImage);
+        imageView.setImage(filterImage.toPrewitt());
+    }
+
+    public Image getCurrentImage() {
+        String file = items.getSelectionModel().getSelectedItem();
+        Path source = Paths.get("src/main/resources/images/" + file);
+        Image image = new Image(source.toFile().toURI().toString());
+        return image;
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public void loadImages() {
+        File directory = Paths.get("src/main/resources/images/").toFile();
+        for (File file : directory.listFiles()) {
+            liste.add(file.getName());
+        }
+        items.setItems(liste);
+    }
+
+}
